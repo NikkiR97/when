d3c1aa3d7c64eb53da4ed77f2a513371db31d782
James Juchau <jhjuchau@gmail.com>
2019-02-21 18:36:02 -0800

    Initial Push

---
+++ Chapter8cpp/runall.bat
@@ -0,0 +1,17 @@
+CALL ..\compile
+CALL ..\run Pascal compile -i assignments.txt
+CALL ..\run Pascal compile -i assignerrors.txt
+CALL ..\run Pascal compile -i repeat.txt
+CALL ..\run Pascal compile -i repeaterrors.txt
+CALL ..\run Pascal compile -i while.txt
+CALL ..\run Pascal compile -i whileerrors.txt
+CALL ..\run Pascal compile -i for.txt
+CALL ..\run Pascal compile -i forerrors.txt
+CALL ..\run Pascal compile -i if.txt
+CALL ..\run Pascal compile -i iferrors.txt
+CALL ..\run Pascal compile -i case.txt
+CALL ..\run Pascal compile -i caseerrors.txt
+CALL ..\run Pascal compile -i loops.txt
+CALL ..\run Pascal compile -i looperrors.txt
+CALL ..\run Pascal compile -i controlsamples.txt
+CALL ..\run Pascal compile -i controlerrors.txt
